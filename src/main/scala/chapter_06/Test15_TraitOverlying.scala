6e608f35c820ae4af1f0ca182d8691f5075b911c
xuyq0618 <xuyq0618@163.com>
2024-07-09 11:13:10 +0800

    面向对象 特征的混入 钻石类型特征

--- src/main/scala/chapter_06/Test15_TraitOverlying.scala
+++ src/main/scala/chapter_06/Test15_TraitOverlying.scala
@@ -8,27 +8,30 @@ object Test15_TraitOverlying {
     println("==========================")
   }
 
-  //球类
-  trait Ball {
-    def describe():String = "ball"
-  }
+}
 
-  //定义颜色特征
-  trait ColorBall extends Ball {
-    var color:String = "red"
-    override def describe():String = "aaaaaaaaaaa:"+color + "-" +super.describe()
-  }
 
-  //定义种类特征
-  trait CategoryBall extends Ball {
-    var cate: String = "foot"
+//球类
+trait Ball {
+  def describe(): String = "ball"
+}
 
-    override def describe(): String = cate + "-" + super.describe()
-  }
+//定义颜色特征
+trait ColorBall extends Ball {
+  var color: String = "red"
 
-  // 自定义球
-  class MyFootBall extends CategoryBall with ColorBall {
-    override def describe(): String = "my ball is:"+super.describe()
-//    override def describe(): String = "my ball is:"+super[CategoryBall].describe() //(指定某个类的方法)
-  }
+  override def describe(): String = "aaaaaaaaaaa:" + color + "-" + super.describe()
+}
+
+//定义种类特征
+trait CategoryBall extends Ball {
+  var cate: String = "foot"
+
+  override def describe(): String = cate + "-" + super.describe()
+}
+
+// 自定义球
+class MyFootBall extends CategoryBall with ColorBall {
+  override def describe(): String = "my ball is:" + super.describe()
+  //    override def describe(): String = "my ball is:"+super[CategoryBall].describe() //(指定某个类的方法)
 }
